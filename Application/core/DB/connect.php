a846a4b0fd7191c15edf2c3d360fd20107f04551
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-07-22 13:28:03 -0300

    Conexão com banco de dados

--- Application/core/DB/connect.php
+++ Application/core/DB/connect.php
@@ -1,1 +1,39 @@
-$
+<?php 
+
+namespace App\Model;
+
+use PDO;
+use PDOException;
+
+class Conexao {
+
+    private static $instancia = null;
+    private $pdo;
+
+    private function __construct()
+    {
+        $host = '127.0.0.1';
+        $dbName = 'storm';
+        $username = 'root';
+        $pass = '';
+        $dsn = "mysql:host=$host;dbname=$dbName;";
+        
+        try {
+            
+            $this->pdo = new PDO(dsn: $dsn, username: $username, password: $pass);
+        } catch (PDOException $e) {
+            
+            die('Erro na conexão: ' . $e->getMessage());
+        }
+    }
+
+    public static function getInstancia(): PDO
+    {
+        if (self::$instancia === null) {
+            self::$instancia = new self();
+        }
+
+        return self::$instancia->pdo;
+    }
+}
+$pdo = Conexao::getInstancia();
