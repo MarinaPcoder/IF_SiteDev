1bbf3a457840a0799d6f91cb84384aa8b99ae639
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-07-22 23:05:38 -0300

    Create Usuario.php

---
+++ App/controllers/Usuario.php
@@ -0,0 +1,31 @@
+<?php
+    namespace App\Controllers;
+
+    class Usuario {
+        private 
+        $nome, 
+        $email, 
+        $nascimento, 
+        $senha, 
+        $bio;
+
+        private function SetNome(): void {
+
+        }
+
+        private function SetEmail(): void {
+
+        }
+
+        private function SetNascimento(): void {
+
+        }
+
+        private function SetSenha(): void {
+
+        }
+
+        private function SetBio(): void {
+
+        }
+    }
