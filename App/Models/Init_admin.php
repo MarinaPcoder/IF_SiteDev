23759ac18ef1fa93c1e4f373fae08a47c2952017
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-09-14 22:18:00 -0300

    Update Init_admin.php

--- App/Models/Init_admin.php
+++ App/Models/Init_admin.php
@@ -1,45 +1,45 @@
 
  <?php 
 
-//     require_once '../../vendor/autoload.php';
+    require_once '../../vendor/autoload.php';
 
-//     use App\Core\DB\Conexao;
+    use App\Core\DB\Conexao;
 
-//     $pdo = Conexao::getInstancia();
-//     $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
+    $pdo = Conexao::getInstancia();
+    $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
 
-//     $nome  = 'Pedro';
-//     $email = 'pedrohtpgbi2007@gmail.com';
-//     $senha = 'Pedro!13579';
-//     $senhamd5  = md5(string: $senha);
+    $nome  = 'Pedro';
+    $email = 'pedrohtpgbi2007@gmail.com';
+    $senha = 'Pedro!13579';
+    $senhamd5  = md5(string: $senha);
 
-// try {
-//     $pdo->beginTransaction();
+try {
+    $pdo->beginTransaction();
 
-//     // já existe?
-//     $q = $pdo->prepare("SELECT COUNT(*) FROM Usuario WHERE tipo_perfil = 'admin'");
-//     $q->execute();
-//     if ($q->fetchColumn() > 0) {
-//         $pdo->rollBack();
-//         exit("Já existe admin. Nada a fazer.\n");
-//     }
+    // já existe?
+    $q = $pdo->prepare("SELECT COUNT(*) FROM Usuario WHERE tipo_perfil = 'admin'");
+    $q->execute();
+    if ($q->fetchColumn() > 0) {
+        $pdo->rollBack();
+        exit("Já existe admin. Nada a fazer.\n");
+    }
 
-//     $sql = "
-//         INSERT INTO Usuario (nome_usuario, email, senha, tipo_perfil, status_ativo)
-//         VALUES (:nome, :email, :senha, 'admin', 1)
-//     ";
-//     $stmt = $pdo->prepare($sql);
-//     $stmt->execute([
-//         ':nome'  => $nome,
-//         ':email' => $email,
-//         ':senha' => $senhamd5,
-//     ]);
+    $sql = "
+        INSERT INTO Usuario (nome_usuario, email, senha, tipo_perfil, status_ativo)
+        VALUES (:nome, :email, :senha, 'admin', 1)
+    ";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        ':nome'  => $nome,
+        ':email' => $email,
+        ':senha' => $senhamd5,
+    ]);
 
-//     $pdo->commit();
-//     echo "Admin criado com sucesso.\n";
-// } catch (Throwable $e) {
-//     if ($pdo->inTransaction()) $pdo->rollBack();
-//     throw $e;
-// }
-// header(header: 'Location: ../../public');
-// exit;
+    $pdo->commit();
+    echo "Admin criado com sucesso.\n";
+} catch (Throwable $e) {
+    if ($pdo->inTransaction()) $pdo->rollBack();
+    throw $e;
+}
+header('Location: ../../public');
+exit;
